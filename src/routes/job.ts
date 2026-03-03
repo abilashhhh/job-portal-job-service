1d77e6c25d57afcca7ac2766e84a12b3df66b77a
Abilash N <abilashnarayanan2001@gmail.com>
2026-03-03 16:17:52 +0530

    Get all active jobs and single jobs

--- src/routes/job.ts
+++ src/routes/job.ts
@@ -1,5 +1,14 @@
 import express from "express";
-import { createCompany, createJob, deleteCompany, getAllCompany, getSingleCompany, updateJob } from "../controllers/job.js";
+import {
+  createCompany,
+  createJob,
+  deleteCompany,
+  getAllActiveJobs,
+  getAllCompany,
+  getSingleCompany,
+  getSingleJob,
+  updateJob,
+} from "../controllers/job.js";
 import { isAuth } from "../middleware/auth.js";
 import uploadFile from "../middleware/multer.js";
 
@@ -9,7 +18,9 @@ router.post("/createCompany", isAuth, uploadFile, createCompany);
 router.delete("/deleteCompany/:companyId", isAuth, deleteCompany);
 router.post("/createJob", isAuth, createJob);
 router.put("/updateJob/:jobId", isAuth, updateJob);
-router.get("/getCompany/all",isAuth, getAllCompany);
+router.get("/getCompany/all", isAuth, getAllCompany);
 router.get("/getCompany/:companyId", isAuth, getSingleCompany);
+router.get("/getAllJobs/active", getAllActiveJobs);
+router.get("/getSingleJob/:jobId", getSingleJob);
 
 export default router;
